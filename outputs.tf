72439f47a0c9cb43f70b283af8d9aa0ccbf1813a
shivanbgtsi <shivanagoudabg@gmail.com>
2024-06-02 10:35:14 +0530

    modifying resource

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
 output "instance_private_ip" {
   description = "Private Ip of the EC2 instance(s)"
-  value       = aws_instance.test_env_ec2[*].private_ip
+  value       = aws_instance.ec2_instance[*].private_ip
 
 }
 
 output "instance_public_ip" {
   description = "Public Ip of the EC2 instance(s)"
-  value       = aws_instance.test_env_ec2[*].public_ip
+  value       = aws_instance.ec2_instance[*].public_ip
 }
 
 #output "kms_key" {
