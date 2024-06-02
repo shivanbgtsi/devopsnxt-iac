54860c6feed96eb0afa320e3b6134f246c4276e8
shivanbgtsi <shivanagoudabg@gmail.com>
2024-06-02 09:54:14 +0530

    Adding jenkins file

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,13 @@
 #Ec2 Instance
-resource "aws_instance" "test_env_ec2" {
+resource "aws_instance" "ec2_instance" {
   ami                         = "ami-00fa32593b478ad6e"
   instance_type               = var.instance_type
 #  key_name                    = var.key_pair_name
-  security_groups             = ["${aws_security_group.security.id}"]
+  security_groups             = [aws_security_group.security.id]
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.subnet.id
   user_data = file("userdata.sh")
   tags = {
-    Name = "Test-with-user-data-new"
+    Name = "DevopsNxt-EC2-Instance"
   }
 }
